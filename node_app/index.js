55e211c578e3f3f2e4bb7d2cea4c723399796349
5164035 <5164035@liae.edu.uniube.br>
2025-09-16 20:47:56 -0300

    delte clientes

--- node_app/index.js
+++ node_app/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = 3001;
-
+app.use(express.json());
 // Configuração do MySQL (igual ao docker-compose)
 const dbConfig = {
   host: "mysql", // nome do serviço no docker-compose
@@ -14,17 +14,6 @@ const dbConfig = {
 
 app.get("/", (req, res) => {
   res.json({ message: "Node.js está rodando no Docker!" });
-});
-
-app.get("/api/v1/clientes", async (req, res) => {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows] = await connection.execute("SELECT * FROM clientes");
-    await connection.end();
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
 });
 
 app.get("/api/v1/clientes/:id", async (req, res) => {
@@ -41,6 +30,65 @@ app.get("/api/v1/clientes/:id", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+app.get("/api/v1/clientes", async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute("SELECT * FROM clientes");
+    await connection.end();
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/api/v1/cliente", async (req, res) => {
+  try {
+    const { nome, email, telefone } = req.body;
+
+    if (!nome || !email || !telefone) {
+      return res
+        .status(400)
+        .json({ error: "Campos obrigatórios: nome, email, telefone" });
+    }
+
+    const connection = await mysql.createConnection(dbConfig);
+
+    const [result] = await connection.execute(
+      "INSERT INTO clientes (nome, email, telefone) VALUES (?, ?, ?)",
+      [nome, email, telefone]
+    );
+
+    await connection.end();
+
+    res.status(201).json({
+      message: "Cliente criado com sucesso!",
+      clienteId: result.insertId,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete("/api/v1/cliente/:id", async (req, res) => {
+  try {
+    const cliente = req.params.id;
+
+    const connection = await mysql.createConnection(dbConfig);
+    await connection.execute("delete from vendas where cliente_id = ?", [
+      cliente,
+    ]);
+
+    const [rows] = await connection.execute(
+      "delete from clientes where id = ?",
+      [cliente]
+    );
+
+    await connection.end();
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor Node rodando na porta ${PORT}`);
+  console.log("servidor na porta $(PORT)");
 });
